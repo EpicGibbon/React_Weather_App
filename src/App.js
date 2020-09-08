ae94a20cb716ddd0fb857ec98d9f798f7412b327
EpicGibbon <michant1997@gmail.com>
2020-09-08 11:59:09 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const API = {
+  key: "c13c64c10390eb9b4417eb4f94f64ca1",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+
+
+  const dateSelector = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August",
+      "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search.."
+          />
+        </div>
+        <div className="location-area">
+          <div className="location"></div>
+          <div className="date">{dateSelector(new Date())}</div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
